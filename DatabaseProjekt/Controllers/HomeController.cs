06b185c9893783ce8f2b0ba11d253defc002cf06
Andreas <andreasleonhard@hotmail.com>
2019-05-29 18:40:43 +0200

    added a github repository

---
+++ DatabaseProjekt/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DatabaseProjekt.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseProjekt.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            ReadAndInsert readfile = new ReadAndInsert();
+            readfile.ReadAllBooks();
+            readfile.GetTownList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ReadAndInsert readfile = new ReadAndInsert();
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ReadAndInsert readfile = new ReadAndInsert();
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DatabaseProjekt/Database/ReadAndInsert.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Data.OleDb;
+
+
+namespace DatabaseProjekt.Database
+{
+    public class ReadAndInsert
+    {
+      
+        //danner en liste af stierne til byerne.
+        public string[] GetFilePaths() {
+            return Directory.GetFiles(@"E:\Skoleprojekter\testdata\testbooks");
+        }
+
+        //Henter alle engelske bynavne ind og lægger dem i en liste.
+        public List<string> GetTownList() {
+
+            using (var reader = new StreamReader(@"E:\Skoleprojekter\testdata\testtowns\towns.csv"))
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('"');
+
+                    listA.Add(values[3]);                   
+                }
+                return listA;
+            }
+        }
+
+        public void ReadAllBooks() {
+
+            string[] bookPaths = GetFilePaths();
+            foreach (var bookPath in bookPaths)
+            {             
+                IEnumerable<bool> authorResults = File.ReadAllLines(@bookPath).Select(s => s.Contains("Author:"));
+                IEnumerable<bool> titleResults = File.ReadAllLines(@bookPath).Select(s => s.Contains("Title:"));
+                string author = FindAuthor(authorResults, bookPath);
+                string title = FindTitle(titleResults, bookPath);
+            }
+            
+        }
+
+        //Finder author for en bog
+        public string FindAuthor(IEnumerable<bool> authorResults, string bookPath)
+        {
+            string[] book = File.ReadAllLines(@bookPath);
+            int authorLineNumber = 0;
+            foreach (var authorResult in authorResults)
+            {
+                if (authorResult)
+                {
+                    string authorLineResult = book[authorLineNumber];
+                    return authorLineResult.Substring(authorLineResult.IndexOf("Author: ") + "Author: ".Length);                                      
+                }
+                authorLineNumber = authorLineNumber + 1;
+            }
+            return "No author found";
+        }
+
+        //finder title for  en bog
+        public string FindTitle(IEnumerable<bool> titleResults, string bookPath)
+        {
+            string[] book = File.ReadAllLines(@bookPath);
+            int titleLineNumber = 0;
+            foreach (var titleResult in titleResults)
+            {
+                if (titleResult)
+                {
+                    string authorLineResult = book[titleLineNumber];
+                    return authorLineResult.Substring(authorLineResult.IndexOf("Title: ") + "Title: ".Length);                   
+                }
+                titleLineNumber = titleLineNumber + 1;
+            }
+            return "No title found";
+        }
+
+    }
+}
